9056c5c67d5868fe94e13b7fa4bed5e374d53b97
Aandikei <andiahmad9288@gmail.com>
2025-01-13 23:02:03 +0700

    update profile dan js usergreeting

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// function ubahNama(){
+//     let nama = prompt("Masukkan Nama Anda", "");
+//     document.getElementById("userGreeting").innerHTML = nama
+// }
+
+// ubahNama()
+
+// let changename = document.getElementById("btnName").addEventListener("click", ubahNama)
