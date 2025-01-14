e78063254263b19abd4f5cdff446fedbd5691ea1
Aandikei <andiahmad9288@gmail.com>
2025-01-14 23:54:20 +0700

    final

--- js/script.js
+++ js/script.js
@@ -1,8 +1,55 @@
-// function ubahNama(){
-//     let nama = prompt("Masukkan Nama Anda", "");
-//     document.getElementById("userGreeting").innerHTML = nama
-// }
+// greeting
+function ubahNama(){
+    let nama = prompt("Masukkan Nama Anda", "");
+    document.getElementById("userGreeting").innerHTML = nama
+}
 
-// ubahNama()
+ubahNama()
 
-// let changename = document.getElementById("btnName").addEventListener("click", ubahNama)
+let changename = document.getElementById("btnName").addEventListener("click", ubahNama)
+
+// slide show
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("imgSlideShow");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex-1].style.display = "block";
+}
+
+setInterval(() => {
+    plusSlides(1)
+}, 2000);
+
+// validation form
+function validateForm() {
+    const name = document.forms["messageForm"]["fullName"].value;
+    const birthDate = document.forms["messageForm"]["birthDate"].value;
+    const gender = document.forms["messageForm"]["gender"].value;
+    const message = document.forms["messageForm"]["message"].value;
+
+    if (name == "" || birthDate == "" || gender == "" || message == "") {
+        alert("Tidak boleh ada yang kosong");
+        return false;
+    }
+
+    setSenderUI(name, birthDate, gender, message);
+    return false;
+}
+
+function setSenderUI(name, birthDate, gender, message) {
+    document.getElementById("senderfullName").innerHTML = name;
+    document.getElementById("senderbirthDate").innerHTML = birthDate;
+    document.getElementById("senderGender").innerHTML = gender;
+    document.getElementById("senderMessage").innerHTML = message;
+}
